0a97ec19625b2e91d5e1febadf3a441ea572b405
dmitrijtrudradost-max <dmitrijtrudradost@gmail.com>
2026-08-23 18:57:05 +0300

    add main.py in track

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+url = "https://speedtest.ru"
+timeout1 = 5
+# Выводим контент страницы
+try:
+    response1 = requests.get(url, timeout = timeout1)
+    statusget1 = response1.status_code
+    print(f"Ответ запроса {statusget1} к адрессу {url}")
+
+    content_type = response1.headers.get('Content-Type', '')
+
+    if statusget1 == 200 and 'application/json' in content_type:
+        data = response1.json()
+        print("Заголовок Content-Type:", data)
+        print("Первые 100 символов JSON:", str(data)[:100])
+
+    elif statusget1 != 200:
+        error_messages = {
+            400: "Ошибка клиента: неверный запрос",
+            403: "Ошибка доступа: запрещено",
+            404: "Ресурс не найден",
+            500: "Внутренняя ошибка сервера",
+            
+        }
+        print(f"Ошибка: {error_messages.get(statusget1, 'Неизвестная ошибка')}")
+
+    else:
+        # Для HTML-страницы выводим первые 300 символов
+        print("Первые 300 символов ответа:", response1.text[:300])
+
+except requests.exceptions.Timeout:
+    print(f"Тайм-аут запроса > {timeout1} сек : сервер {url} не ответил вовремя")
+
+except requests.exceptions.ConnectionError:
+    print(f"Нет соединения: ошибка сети (DNS, недоступность хоста {url} и т.п.)")
